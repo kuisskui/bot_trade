0b31877064acd91f848fca300ca4461dd35355d0
zkaopong.s <zkaopong.s@motiftech.com>
2024-10-06 15:01:21 +0700

    make strategy class as a caller to call a script

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -1,7 +1,19 @@
+import subprocess
+import os
+
+
 class Strategy:
-    def __init__(self):
-        self.symbol = None
-        self.time_frame = None
-        self.lot = None
-        self.signal = None
-        self.position = None
+    def __init__(self, script):
+        self.script = os.path.join(os.path.dirname(__file__), script)
+
+    def get_signal(self, json):
+        try:
+            result = subprocess.run(
+                ["python", self.script, json],
+                capture_output=True,
+                text=True
+            )
+            print(result.stdout)
+            return result.stdout
+        except subprocess.CalledProcessError as e:
+            print("Error: ", e.stderr)
