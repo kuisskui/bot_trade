ea6bd25d94a6f1706d5279e76d1491efa9359175
zkaopong.s <zkaopong.s@motiftech.com>
2024-10-09 13:22:42 +0700

    save

--- bot/bot_trade.py
+++ bot/bot_trade.py
@@ -1,10 +1,13 @@
+import script.mt5.mt5_api as mt5
+
+
 class BotTrade:
     def __init__(self, bot_id):
         self.bot_id: int = bot_id
         self.position_ticket = None
 
     def trade(self, signal):
-        print(f"Bot: {self.bot_id} Trade: {signal}")
+        self.position_ticket = mt5.place_trade("BTCUSD", 0.01, signal)
 
     def update(self, signal):
         self.trade(signal)
